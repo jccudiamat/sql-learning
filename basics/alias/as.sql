902aa00037f9af3f162356db5310ae34046a47c4
Jessie <jccudiamat55@gmail.com>
2025-01-17 00:24:09 +0400

    Modify Alias as note

--- basics/alias/as.sql
+++ basics/alias/as.sql
@@ -1,10 +1,10 @@
 SELECT
-	job_id,
-	job_title_short,
+	job_id AS ID,
+	job_title_short AS Title,
 	job_via AS 'Site',
-	salary_year_avg,
-	job_location
+	salary_year_avg AS Salary,
+	job_location AS Location
 FROM
-	job_postings_fact
+	job_postings_fact AS jpf
 WHERE 
 	job_title LIKE '%Data%';

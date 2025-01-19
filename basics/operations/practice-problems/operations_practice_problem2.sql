876fcc88fab0880643b521775ed42bb8b0ffcf3d
Jessie <jccudiamat55@gmail.com>
2025-01-19 22:37:22 +0400

    Division Operation

---
+++ basics/operations/practice-problems/operations_practice_problem2.sql
@@ -0,0 +1,12 @@
+Question:
+In the invoices_fact table divide the hours_spent by the hours_rate. Return the 
+activity_id as well. This helps give us an efficiency ration a lower ratio could 
+indicate a more cost-efficient task.
+Solution:
+SELECT 
+	activity_id,
+	hours_spent,
+	hours_rate,
+	hours_spent / hours_rate AS 'Efficiency ratio'
+FROM
+	invoices_fact 
